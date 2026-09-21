058d740cc1de46dbe3798d7ca3058646aeaa980d
MrClokOut <mr.clokout@gmail.com>
2026-09-22 00:10:59 +0500

    js added for optimization

---
+++ levels/js.js
@@ -0,0 +1,9 @@
+function quest(isCorrect){
+    if(isCorrect === true){
+        alert("Правильно!")
+    }
+    else{
+        alert("Неправильно!")
+    }
+    window.location.href = '../center/kvis.html';
+}
